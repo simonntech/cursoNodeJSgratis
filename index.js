83bb234f0a5a34480230e9073c072ea980a0faa0
Bruno Simon <contato.simon.tech@gmail.com>
2025-06-26 14:25:06 -0300

    aula 1 a 3

---
+++ index.js
@@ -0,0 +1,19 @@
+// as variáveis devem ter um nome explicativo, comentários são úteis apenas em alguns casos específicos
+
+const rl = require("node:readline");
+
+const prompt = rl.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+})
+
+let name;
+
+prompt.question("Qual é o seu nome? ", answer => {
+    name = answer;
+    prompt.close();
+})
+
+prompt.on("close", () => {
+    console.log(`Seja bem vindo(a) ${name}`)
+})
